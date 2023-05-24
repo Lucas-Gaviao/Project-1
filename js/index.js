5cc16991b092f7729974c97c114f19a3a398baff
Lucas Silva <gaviao.lucas@gmail.com>
2023-05-24 08:12:22 +0100

    partial shooting function

--- js/index.js
+++ js/index.js
@@ -4,52 +4,62 @@ class Game {
 
         this.player = null
         this.objectArr = [];
-
     }
 
     startGame(){
 
         this.player = new Player();
         this.attachEventListeners();
+        this.object = new Objects();
 
         setInterval(() => {
-            const newAlien = new Objects();
-            this.objectArr.push(newAlien);
+            
+            this.objectArr.push(object);
         },800)
       
         setInterval(() =>{
-          this.objectArr.forEach((alien) => {
-            alien.moveDown();
-
-
-            this.detectCollision(alien);
-
-            this.deleteObject(alien);
+          this.objectArr.forEach((object) => {
+            object.moveDown();
+            this.detectCollision(object);
+            this.deleteObject(object);
           });  
         }, 100);
     }
+
     attachEventListeners() {
         document.addEventListener("keydown", (e) => {
             if(e.code === "ArrowLeft" && this.player.positionX - this.player.playerSpeed >= 0){
                 this.player.moveLeft();
+                this.detectCollision();
             } else if(e.code === "ArrowRight" && this.player.positionX + this.player.playerSpeed <= 100){
                 this.player.moveRight();
-            } 
+                this.detectCollision();
+            } else if(e.code === "ArrowUp" && this.player.positionY - this.player.playerSpeed <= 100){
+                this.player.moveUp();
+                this.detectCollision();
+            } else if(e.code === "ArrowDown" && this.player.positionY - this.player.playerSpeed >= 0){
+                this.player.moveDown();
+                this.detectCollision();
+            } else if(e.code === "SpaceBar"){
+                this.bullet.shootUp();
+                alert(e.code)
+            }
           })  
     }
-    detectCollision(alien){
-        if (alien.positionX < this.player.positionX + this.player.width &&
-            alien.positionX + alien.width > this.player.positionX &&
-            alien.positionY < this.player.positionY + this.player.height &&
-            alien.height + alien.positionY > this.player.positionY) {
+    detectCollision(object){
+        
+        if (this.object.positionX < this.player.positionX + this.player.width &&
+            object.positionX + object.width > this.player.positionX &&
+            object.positionY < this.player.positionY + this.player.height &&
+            object.height + object.positionY > this.player.positionY) {
                 location.href = "/game-over.html";
             } 
-        }
+    }
     
-    deleteObject(alien){
-        if(alien.positionX < 0 - alien.height){
+    deleteObject(object){
+        if(object.positionX < 0 - object.height){
             //remove element from the dom;
-            alien.DomEl.remove();
+            object.DomEl.remove();
             //remove the element from the array;
             this.objectArr.shift();
         }
@@ -59,11 +69,11 @@ class Game {
 class Player {
 
     constructor(){
-        this.height = 10;
+        this.height = 3;
         this.width = 3;
         this.positionX = 50 - this.height / 2;
-        this.positionY = 0
-        this.playerSpeed = 3;
+        this.positionY = 10
+        this.playerSpeed = 2;
 
         this.DomEl = null;
 
@@ -83,7 +93,7 @@ class Player {
         parent.appendChild(this.DomEl);
     }  
     
-  /*  moveUp() {
+    moveUp() {
         this.positionY += this.playerSpeed;
         this.DomEl.style.bottom = this.positionY + "vh";
     }
@@ -93,7 +103,7 @@ class Player {
         this.DomEl.style.bottom = this.positionY + "vh"
 
     }
-*/
+
     moveLeft() {
         if(this.positionX > 0 + this.width){
             this.positionX -= this.playerSpeed;
@@ -112,8 +122,8 @@ class Player {
 class Objects {
 
     constructor(){
-        this.height = 8;
-        this.width = 10;
+        this.height = 4;
+        this.width = 8;
         this.positionX = Math.floor(Math.random() * (100 - this.width));
         this.positionY = 100;
         this.objectSpeed = 2;
@@ -135,7 +145,6 @@ class Objects {
 
         const parent = document.getElementById("space");
         parent.appendChild(this.DomEl);
-        
     }
 
     moveDown() {
@@ -146,10 +155,38 @@ class Objects {
   
 }
 
-class PowerObj{
+class Bullet extends Game {
     constructor(){
+        this.height = 1;
+        this.width = 1;
+        this.bulletX = this.player.positionX;
+        this.bulletY = this.player.positionY;
+        this.bulletSpeed = 2;
 
+        this.domElBullet = null;
+
+        this.createDomElement();
     }
+
+    createDomElement(){
+
+        this.domElBullet = document.createElement("div");
+        this.domElBullet.id = "bullet";
+
+        this.domElBullet.style.height = this.height + "vh";
+        this.domElBullet.style.width = this.width + "vw";
+        this.domElBullet.style.left = this.bulletX + "vw";
+        this.domElBullet.style.bottom = this.bulletY + "vh";
+
+        const parentEl = document.getElementById("space");
+        parentEl.appendChild(this.domElBullet);
+    }
+
+    shootUp(){
+        this.bulletY += this.bulletSpeed;
+        this.domElBullet.style.bottom = this.bulletY + "vh"
+    }
+
 }   
 
 
